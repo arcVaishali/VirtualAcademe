83c6984f0e8c658e9bfa2dd64555ee72088bd6b7
Vidip Ghosh <vidip2001@gmail.com>
2023-10-06 23:13:04 +0530

    Firebase Added

---
+++ client/src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaqlko7ZHc6XM5HWNzIA5y6vZ49FY2HXA",
+  authDomain: "virtual-campus-7bd37.firebaseapp.com",
+  projectId: "virtual-campus-7bd37",
+  storageBucket: "virtual-campus-7bd37.appspot.com",
+  messagingSenderId: "815469823017",
+  appId: "1:815469823017:web:031116df7e820a94e48838"
+};
+
+export const app = initializeApp(firebaseConfig);
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Home from "./Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "signup",
+    element: <SignUp/>
+  },
+  {
+    path: "login",
+    element: <Login/>
+  },
+  {
+    path: '/',
+    element: <Home/>
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
