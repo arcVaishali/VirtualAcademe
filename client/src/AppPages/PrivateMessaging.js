a692ae485d5120163646937d59417a57afe4cbdc
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-08 00:51:41 +0530

    real time messaging

--- client/src/AppPages/PrivateMessaging.js
+++ client/src/AppPages/PrivateMessaging.js
@@ -7,8 +7,10 @@ import { onSnapshot } from 'firebase/firestore';
 const PrivateMessaging = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0(); 
-  
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const [selectedUser, setSelectedUser] = useState(null); 
+  const users = ['Vaishnavi', 'Vidip', 'Vaishali','Maruthi']; 
+
   useEffect(() => {
     if (isAuthenticated) {
       const q = collection(db, 'messages');
@@ -29,24 +31,25 @@ const PrivateMessaging = () => {
 
   const sendMessage = async (e) => {
     e.preventDefault();
-  
+
     if (!isAuthenticated) {
       loginWithRedirect();
       return;
     }
-  
+
     if (message.trim() === '') {
       alert('Enter a valid message');
       return;
     }
-  
-    const userEmail = user.email; 
-  
+
+    const userEmail = user.email;
+
     try {
       await addDoc(collection(db, 'messages'), {
         text: message,
-        name: userEmail, 
+        name: userEmail,
         createdAt: serverTimestamp(),
+        receiver: selectedUser,
       });
       setMessage('');
     } catch (error) {
@@ -54,34 +57,57 @@ const PrivateMessaging = () => {
     }
   };
 
+  // Filter messages based on the selected user
+  const filteredMessages = messages.filter(
+    (msg) =>
+      (msg.name === user.email && msg.receiver === selectedUser) ||
+      (msg.name === selectedUser && msg.receiver === user.email)
+  );
+
   return (
-    <div>
-      {isAuthenticated ? (
-        <div>
-          <button onClick={() => logout()}>Log Out</button>
-          <form onSubmit={(e) => sendMessage(e)} className="send-messages">
-            <label htmlFor="messageInput">Enter Message</label>
-            <input
-              value={message}
-              type="text"
-              onChange={(e) => setMessage(e.target.value)}
-              id="messageInput"
-              name="messageInput"
-              placeholder="Type a message"
-            />
-            <button type="submit">Send</button>
-          </form>
-          <div className="messages">
-            {messages.map((msg) => (
-              <div key={msg.id}>
-                <strong>{msg.name}</strong>: {msg.text}
-              </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        <button onClick={() => loginWithRedirect({ appState: { returnTo: '/messages' } })}>Log In</button>
-      )}
+    <div className="chat-app">
+      <div className="user-list">
+        <h2>Users</h2>
+        <ul>
+          {/* Display a list of users and handle user selection */}
+          {users.map((u) => (
+            <li
+              key={u}
+              onClick={() => setSelectedUser(u)}
+              className={u === selectedUser ? 'selected' : ''}
+            >
+              {u}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="chat">
+        {selectedUser && (
+          <>
+            <h2>Chat with {selectedUser}</h2>
+            <div className="messages">
+              {filteredMessages.map((msg) => (
+                <div
+                  key={msg.id}
+                  className={msg.name === user.email ? 'sent' : 'received'}
+                >
+                  <strong>{msg.name}</strong>: {msg.text}
+                </div>
+              ))}
+            </div>
+            <form onSubmit={(e) => sendMessage(e)} className="send-messages">
+              <input
+                value={message}
+                type="text"
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Type a message"
+              />
+              <button type="submit">Send</button>
+            </form>
+            <button onClick={() => logout()}>Log Out</button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
