860ebc837032a3228116674f6dcb271a014f05c8
vaishali <vaishalirdj14@gmail.com>
2023-10-07 17:36:31 +0530

    private messaging ui almost complete

--- client/src/AppPages/Login.js
+++ client/src/AppPages/Login.js
@@ -1,67 +1,67 @@
-import React,{useState} from 'react'
-import { signInWithEmailAndPassword,getAuth } from 'firebase/auth'
-import { app } from '../Firebase'; 
-import {  useNavigate} from 'react-router-dom'
+// import React,{useState} from 'react'
+// import { signInWithEmailAndPassword,getAuth } from 'firebase/auth'
+// import { app } from '../Firebase'; 
+// import {  useNavigate} from 'react-router-dom'
 
-const firebaseAuth = getAuth(app);
+// const firebaseAuth = getAuth(app);
 
-const Login = () => {
-    const navigate = useNavigate()
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('')
-    const onLogin = async(e) =>{
-        e.preventDefault();
-        await signInWithEmailAndPassword(firebaseAuth,email,password).then((userCredential)=>{
-            const user = userCredential.user;
-            navigate('/')
-            console.log(user);
-        }).catch((e)=>{
-            console.log('Error');
-        })
-    }
-  return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
-                   Sign in
-                </h1>
-                <form className="mt-6">
-                    <div className="mb-2">
-                        <label
-                            for="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Email
-                        </label>
-                        <input className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40" type="email" label="Email Address" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email Holder' />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Password
-                        </label>
-                        <input className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40" type="password" label="Create Password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Password' />
-                    </div>
-                    <div className="mt-6">
-                      <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600" type='submit' onClick={onLogin}>LogIn</button>
-                    </div>
-                </form>
+// const Login = () => {
+//     const navigate = useNavigate()
+//     const [email,setEmail] = useState('');
+//     const [password,setPassword] = useState('')
+//     const onLogin = async(e) =>{
+//         e.preventDefault();
+//         await signInWithEmailAndPassword(firebaseAuth,email,password).then((userCredential)=>{
+//             const user = userCredential.user;
+//             navigate('/')
+//             console.log(user);
+//         }).catch((e)=>{
+//             console.log('Error');
+//         })
+//     }
+//   return (
+//     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+//             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+//                 <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+//                    Sign in
+//                 </h1>
+//                 <form className="mt-6">
+//                     <div className="mb-2">
+//                         <label
+//                             for="email"
+//                             className="block text-sm font-semibold text-gray-800"
+//                         >
+//                             Email
+//                         </label>
+//                         <input className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40" type="email" label="Email Address" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email Holder' />
+//                     </div>
+//                     <div className="mb-2">
+//                         <label
+//                             for="password"
+//                             className="block text-sm font-semibold text-gray-800"
+//                         >
+//                             Password
+//                         </label>
+//                         <input className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40" type="password" label="Create Password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Password' />
+//                     </div>
+//                     <div className="mt-6">
+//                       <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600" type='submit' onClick={onLogin}>LogIn</button>
+//                     </div>
+//                 </form>
 
-                <p className="mt-8 text-xs font-light text-center text-gray-700">
-                    {" "}
-                    Don't have an account?{" "}
-                    <a
-                        href="/signup"
-                        className="font-medium text-purple-600 hover:underline"
-                    >
-                        Sign up
-                    </a>
-                </p>
-            </div>
-        </div>
-  )
-}
+//                 <p className="mt-8 text-xs font-light text-center text-gray-700">
+//                     {" "}
+//                     Don't have an account?{" "}
+//                     <a
+//                         href="/signup"
+//                         className="font-medium text-purple-600 hover:underline"
+//                     >
+//                         Sign up
+//                     </a>
+//                 </p>
+//             </div>
+//         </div>
+//   )
+// }
 
-export default Login
+// export default Login
--- client/src/AppPages/PrivateMessaging.js
+++ client/src/AppPages/PrivateMessaging.js
@@ -1,20 +1,109 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const PrivateMessaging = () => {
-    const navigation = [ "Chats" , "Groups" ]
+  const navigation = ["Chats", "Groups"];
+  const chatList = [
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+    {
+      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      name: "Anna",
+      message: "See you soon",
+      time: "7:00PM",
+    },
+  ];
   return (
-    <div className="grid grid-cols-12 justify-center items-center px-8 py-8">
+    <div className="grid grid-cols-12 justify-center items-center px-4 py-4 ">
+      {/* HEADER */}
+      <div className="grid grid-cols-12 col-span-12 justify-center items-center px-4 py-4 border-b-[1px] border-gray-200">
         <div className="flex">
-            <img
+          <img
             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
-            className='w-[50px] h-[50px] object-cover rounded-[25px]'
-            />
+            className="w-[50px] h-[50px] object-cover rounded-[25px]"
+          />
         </div>
-        <div className="grid grid-cols-12 col-span-10 col-start-3">
-            
+        <div className="grid grid-cols-12 col-span-10 col-start-2">
+          {navigation.map((key, index) => (
+            <Link to="/" className="text-black font-thin">
+              {navigation[index]}
+            </Link>
+          ))}
         </div>
+      </div>
+      {/* CHAT LIST */}
+      <div className="grid grid-rows-1 col-span-4 border-r-[1px] border-gray-100 rounded-sm mt-8">
+        {chatList.map((element) => (
+          <div className="grid grid-cols-6 col-span-6 px-2 py-2 justify-between items-center border-b-[1px] border-gray-300">
+            <div className="grid grid-cols-6 col-span-4">
+              <img
+                src={element.icon}
+                className="w-[50px] h-[50px] object-cover rounded-[25px] col-span-2"
+              />
+              <div className="grid grid-rows-1 col-span-3">
+                <span className="text-base text-extralight">
+                  {element.name}
+                </span>
+                <span className="text-sm text-extralight">
+                  {element.message}
+                </span>
+              </div>
+            </div>
+            <span className="text-xs">{element.time}</span>
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-8 col-span-8">
+        {/* CHAT BODY */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PrivateMessaging
+export default PrivateMessaging;
