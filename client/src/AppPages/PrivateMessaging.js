3207d2ce3d71b6ab1e0d88bf160ca5ad51437f43
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-08 23:10:41 +0530

    private messaging

--- client/src/AppPages/PrivateMessaging.js
+++ client/src/AppPages/PrivateMessaging.js
@@ -1,105 +1,97 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { FaRegPaperPlane } from "react-icons/fa";
 
 const PrivateMessaging = () => {
-  const navigation = ["Chats", "Groups"];
-  const chatList = [
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-    {
-      icon: "https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      name: "Anna",
-      message: "See you soon",
-      time: "7:00PM",
-    },
-  ];
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=15")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.results);
+      });
+  }, []);
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() === "" || !selectedUser) return;
+
+    const message = {
+      text: newMessage,
+      sender: "You",
+      timestamp: new Date().toLocaleTimeString(),
+    };
+
+    setMessages([...messages, message]);
+    setNewMessage("");
+  };
+
   return (
-    <div className="grid grid-cols-12 justify-center items-center px-4 py-4 ">
-      {/* HEADER */}
-      <div className="grid grid-cols-12 col-span-12 justify-center items-center px-4 py-4 border-b-[1px] border-gray-200">
-        <div className="flex">
-          <img
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
-            className="w-[50px] h-[50px] object-cover rounded-[25px]"
-          />
-        </div>
-        <div className="grid grid-cols-12 col-span-10 col-start-2">
-          {navigation.map((key, index) => (
-            <Link to="/" className="text-black font-thin">
-              {navigation[index]}
-            </Link>
-          ))}
-        </div>
-      </div>
-      {/* CHAT LIST */}
-      <div className="grid grid-rows-1 col-span-4 border-r-[1px] border-gray-100 rounded-sm mt-8">
-        {chatList.map((element) => (
-          <div className="grid grid-cols-6 col-span-6 px-2 py-2 justify-between items-center border-b-[1px] border-gray-300">
-            <div className="grid grid-cols-6 col-span-4">
+    <div className="flex h-screen bg-gray-200">
+      {/* User List */}
+      <div className="w-1/4 bg-white border-r border-gray-300 p-4">
+        <h2 className="text-lg font-semibold mb-4">Direct Message</h2>
+        <ul>
+          {users.map((user) => (
+            <li
+              key={user.login.uuid}
+              className={`flex items-center p-2 cursor-pointer ${
+                selectedUser === user ? "bg-gray-200" : ""
+              }`}
+              onClick={() => setSelectedUser(user)}
+            >
               <img
-                src={element.icon}
-                className="w-[50px] h-[50px] object-cover rounded-[25px] col-span-2"
+                src={user.picture.thumbnail}
+                alt={user.name.first}
+                className="w-8 h-8 rounded-full mr-2"
               />
-              <div className="grid grid-rows-1 col-span-3">
-                <span className="text-base text-extralight">
-                  {element.name}
-                </span>
-                <span className="text-sm text-extralight">
-                  {element.message}
-                </span>
-              </div>
-            </div>
-            <span className="text-xs">{element.time}</span>
-          </div>
-        ))}
+              <span>{`${user.name.first} ${user.name.last}`}</span>
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="grid grid-cols-8 col-span-8">{/* CHAT BODY */}</div>
+
+      {/* Chat Window */}
+      <div className="w-3/4 p-4">
+        {selectedUser ? (
+          <div className="h-4/5 bg-white rounded-lg shadow-md p-4">
+            <div className="h-4/5 overflow-y-scroll">
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`mb-4 ${
+                    message.sender === "You" ? "text-right" : "text-left"
+                  }`}
+                >
+                  <span className="font-semibold">{message.sender}:</span>{" "}
+                  {message.text}
+                </div>
+              ))}
+            </div>
+            <div className="mt-2 flex">
+              <input
+                type="text"
+                className="flex-grow border rounded-l-md p-2 focus:outline-none"
+                placeholder="Type a message..."
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+              />
+              <button
+                className="bg-green-500 text-white rounded-r-md p-2 hover:bg-green-600"
+                onClick={handleSendMessage}
+              >
+                <FaRegPaperPlane />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="flex-grow flex items-center justify-center text-gray-500">
+            Select a user to start a chat.
+          </div>
+        )}
+      </div>
     </div>
   );
 };
